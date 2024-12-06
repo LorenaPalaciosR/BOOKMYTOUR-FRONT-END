5cde4d8abb075faf90c1f2124c8b6c734c9a859d
rayrodriguez028 <rayrodriguez028@gmail.com>
2024-12-05 20:45:03 -0600

    HU 24 Marcar como favorito

--- bookmytour-project/src/Components/Card.jsx
+++ bookmytour-project/src/Components/Card.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Styles from '../Styles/Card.module.css'
 import { Link } from 'react-router-dom'
+import { useContextGlobalStates } from '../Components/utils/global.context'
 
-const Card = ({ id, title, img, price, description }) => {
+const Card = ({ id, title, img, price, description }) => { 
+  const { state, dispatch } = useContextGlobalStates();
+  const isFav = state.favs.find((favid) => favid === id);
+  const addFav = () => dispatch({ type: isFav ? "REMOVE_FAV" : "ADD_FAV", payload: id });
   return (
     <div className={Styles.container}>
       <Link key={id} to={`${window.location.origin}/detalle/${id}`}>
@@ -11,6 +15,7 @@ const Card = ({ id, title, img, price, description }) => {
           <h5>$ {price}</h5>
           <p>{description}</p>
       </Link>
+      <button className={Styles.favButton} onClick={addFav}>{isFav ? "❤️" : "🤍"}</button>
     </div>
   )
 }
